b9b2b273ce50e401fe67303c1b87773d03564c52
ShenQingchuan <tmyeve@163.com>
2022-07-24 18:07:55 +0800

    feat(hyperlink): add link URL edit validation

--- packages/app/src/components/editor/hdEditorLinkEdit.vue
+++ packages/app/src/components/editor/hdEditorLinkEdit.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import type { EditorCore, HyperlinkAttrs } from '@hetero/editor'
 import { EditorFloatMenuAction } from '../../constants/editor'
+import { URLRegExp } from '../../constants/regExp'
 
 const props = defineProps<{
   editorCore?: EditorCore
@@ -9,20 +10,8 @@ const props = defineProps<{
 const { t } = useI18n()
 const editorStore = useEditorStore()
 const linkEditRef = ref<HTMLElement>()
+const isEditLinkValid = computed(() => URLRegExp.test(editorStore.linkEditURL))
 
-watch(
-  () => editorStore.isShowLinkEdit,
-  (isNowShowLinkEdit) => {
-    if (!isNowShowLinkEdit) {
-      return
-    }
-    editorStore.setPrevLinkAttrs()
-    if (editorStore.floatMenuByAction === EditorFloatMenuAction.BySelection && props.editorCore) {
-      const { url } = props.editorCore.helpers.getMarkAttrs<HyperlinkAttrs>('hyperlink')
-      editorStore.setLinkEditURL(url)
-    }
-  },
-)
 const confirmHyperlinkMark = () => {
   props.editorCore?.commands.updateHyperlink({
     current: {
@@ -37,6 +26,19 @@ const openLink = () => {
   window.open(editorStore.linkEditURL, '__blank')
   window.opener = null // SAFETY
 }
+watch(
+  () => editorStore.isShowLinkEdit,
+  (isNowShowLinkEdit) => {
+    if (!isNowShowLinkEdit) {
+      return
+    }
+    editorStore.setPrevLinkAttrs()
+    if (editorStore.floatMenuByAction === EditorFloatMenuAction.BySelection && props.editorCore) {
+      const { url } = props.editorCore.helpers.getMarkAttrs<HyperlinkAttrs>('hyperlink')
+      editorStore.setLinkEditURL(url)
+    }
+  },
+)
 </script>
 
 <template>
@@ -56,16 +58,29 @@ const openLink = () => {
       >
         <div class="hetero-editor__link-edit edit-link" flex-items-center p-x-1 min-w-320px mb2>
           <span whitespace-nowrap txt-color-base mr3>{{ t('editor.popover.edit-link-url-label') }}</span>
-          <n-input v-model:value="editorStore.linkEditURL" autofocus type="text" :placeholder="t('editor.popover.edit-link-url-placeholder')" mr2 />
+          <n-input
+            v-model:value="editorStore.linkEditURL"
+            autofocus type="text"
+            :status="isEditLinkValid ? 'success' : undefined"
+            :placeholder="t('editor.popover.edit-link-url-placeholder')" mr2
+          />
           <i i-ri:close-circle-line text-6 cursor-pointer txt-color-base hover:text-emerald-600 @click="editorStore.setShowLinkEdit(false)" />
         </div>
         <div class="hetero-editor__link-edit edit-text" flex-items-center p-x-1 min-w-320px>
           <span whitespace-nowrap txt-color-base mr3>{{ t('editor.popover.edit-link-text-label') }}</span>
           <n-input v-model:value="editorStore.linkEditText" type="text" :placeholder="t('editor.popover.edit-link-text-placeholder')" mr3 />
-          <n-button mr3 class="hetero-editor__link-edit confirm" @click="confirmHyperlinkMark">
+          <n-button
+            mr3 class="hetero-editor__link-edit confirm"
+            :disabled="!isEditLinkValid"
+            @click="confirmHyperlinkMark"
+          >
             {{ t('editor.popover.edit-link-confirm') }}
           </n-button>
-          <n-button v-show="editorStore.linkEditURL" quaternary class="hetero-editor__link-edit open-link" @click="openLink">
+          <n-button
+            v-show="isEditLinkValid"
+            quaternary class="hetero-editor__link-edit open-link"
+            @click="openLink"
+          >
             <template #icon>
               <i i-carbon:link text-6 cursor-pointer txt-color-base hover:text-emerald-600 @click="editorStore.setShowLinkEdit(false)" />
             </template>
