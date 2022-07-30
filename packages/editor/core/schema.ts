597975b4fc1ae0a892e9216b727816dd295417db
ShenQingchuan <tmy1106@outlook.com>
2022-07-30 16:08:25 +0800

    feat(editor): add schema utils for text block

--- packages/editor/core/schema.ts
+++ packages/editor/core/schema.ts
@@ -1,4 +1,47 @@
-import type { SchemaSpec } from 'prosemirror-model'
+import type { AttributeSpec, Node, SchemaSpec } from 'prosemirror-model'
+import { merge, omit } from 'lodash'
+
+export function extendsTextBlockAttrs(
+  others: Record<string, AttributeSpec> = {}, excludes?: string[],
+) {
+  const textBlockAttrsBase: Record<string, AttributeSpec> = {
+    textAlign: { default: 'left' },
+    textIndent: { default: 0 },
+  }
+  let textBlockAttrs = merge(textBlockAttrsBase, others)
+  if (excludes) {
+    textBlockAttrs = omit(textBlockAttrs, ...excludes)
+  }
+  return textBlockAttrs
+}
+
+export function stylesOfTextBlock(
+  node: Node,
+  otherStyle?: string | ((node: Node) => string),
+) {
+  let style = ''
+  if (node.attrs.textAlign) {
+    style += `text-align:${node.attrs.textAlign};`
+  }
+  if (node.attrs.textIndent > 0) {
+    style += `padding-left:${node.attrs.textIndent}em;`
+  }
+
+  if (typeof otherStyle === 'function') {
+    style = style + otherStyle(node)
+  }
+  else {
+    style += otherStyle
+  }
+  return style
+}
+
+export function getTextBlockAttrsFromElement(el: HTMLElement) {
+  return {
+    textAlign: el.style.textAlign ?? 'left',
+    textIndent: el.style.textIndent ?? 0,
+  }
+}
 
 export function mergeSchemaSpecs(specs: Partial<SchemaSpec>[]): SchemaSpec {
   const merged: SchemaSpec = {
@@ -7,8 +50,19 @@ export function mergeSchemaSpecs(specs: Partial<SchemaSpec>[]): SchemaSpec {
       paragraph: {
         group: 'block',
         content: 'inline*',
-        parseDOM: [{ tag: 'p' }],
-        toDOM() { return ['p', 0] },
+        attrs: extendsTextBlockAttrs(),
+        parseDOM: [{
+          tag: 'p',
+          getAttrs(element) {
+            return element instanceof HTMLElement
+              ? getTextBlockAttrsFromElement(element)
+              : null
+          },
+        }],
+        toDOM(node) {
+          const style = stylesOfTextBlock(node)
+          return ['p', { style }, 0]
+        },
       },
       text: { group: 'inline' },
     },
